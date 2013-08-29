3fc0472ac75fd3b77d6c629fef9cf4a2910a34af
Niall O'Higgins <niallo@frozenridge.co>
2013-08-28 18:29:27 -0700

    update for strider 1.4.x compatibility. now depends on strider-detection-rules module.

--- worker.js
+++ worker.js
@@ -1,3 +1,4 @@
+var detect = require('strider-detection-rules')
 var path = require('path')
 
 // We include a copy of the virtualenv script
@@ -8,29 +9,39 @@ var VIRTUAL_ENV_DIR = "venv"
 var VIRTUAL_ENV_CMD = "python " + VIRTUAL_ENV_PY + " " + VIRTUAL_ENV_DIR
 var VIRTUAL_PYTHON = path.join(VIRTUAL_ENV_DIR, "bin", "python")
 var VIRTUAL_PIP = path.join(VIRTUAL_ENV_DIR, "bin", "pip")
+var CREATE_VIRTUAL_ENV = VIRTUAL_ENV_CMD + " && " + VIRTUAL_PIP + " install -r requirements.txt"
+
+var RULES = [{
+  filename:"manage.py",
+  grep:/django/i,
+  language:"python",
+  framework:"django",
+  prepare:CREATE_VIRTUAL_ENV,
+  test:VIRTUAL_PYTHON + " manage.py test",
+}, {
+  filename:"setup.py",
+  exists:true,
+  language:"python",
+  framework:null,
+  prepare:CREATE_VIRTUAL_ENV,
+  test:VIRTUAL_PYTHON + " setup.py test",
+}]
+
 
 module.exports = function(ctx, cb) {
 
-  var CREATE_VIRTUAL_ENV = VIRTUAL_ENV_CMD + " && " + VIRTUAL_PIP + " install -r requirements.txt"
+  function doTest(ctx , cb){
+    detect(RULES, "test", ctx, cb)
+  }
 
-  ctx.addDetectionRule({
-    filename:"manage.py",
-    grep:/django/i,
-    language:"python",
-    framework:"django",
-    prepare:CREATE_VIRTUAL_ENV,
-    test:VIRTUAL_PYTHON + " manage.py test",
+  function doPrepare(ctx, cb) {
+    detect(RULES, "prepare", ctx, cb)
+  }
+
+  ctx.addBuildHook({
+    test: doTest,
+    prepare: doPrepare,
   })
-
-  ctx.addDetectionRule({
-    filename:"setup.py",
-    exists:true,
-    language:"python",
-    framework:null,
-    prepare:CREATE_VIRTUAL_ENV,
-    test:VIRTUAL_PYTHON + " setup.py test",
-  })
-
 
   console.log("strider-python extension loaded")
   cb(null, null)
