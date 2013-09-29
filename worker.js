29310f09c0909985b846547fd592e3d42219d9ac
Jared Forsyth <jared@jaredforsyth.com>
2013-09-29 00:17:55 -0600

    pip install now checks for requirements.txt first

--- worker.js
+++ worker.js
@@ -1,13 +1,21 @@
 var path = require('path')
+  , fs = require('fs')
 
 module.exports = {
   init: function (config, job, context, done) {
-    var venvDir = path.join(context.dataDir, '.venv')
+    var venvDir = path.join(context.baseDir, '.venv')
     done(null, {
       path: [path.join(__dirname, 'thirdparty'),
              path.join(venvDir, 'bin')],
       environment: 'virtualenv.py ' + venvDir,
-      prepare: 'pip install -r requirements.txt',
+      prepare: function (context, done) {
+        if (fs.existsSync(path.join(context.dataDir, 'requirements.txt'))) {
+          return context.cmd('pip install -r requirements.txt', function (err) {
+            done(err, true)
+          })
+        }
+        done(null, false)
+      },
       test: config.test !== 'none' ? config.test : undefined
     })
   },
