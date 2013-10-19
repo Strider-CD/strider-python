38256c18f391ecbe6d04d98f60fea70c2676354d
Niall O'Higgins <niallo@frozenridge.co>
2013-10-19 16:02:55 -0700

    make prepare command support arbitrary shell

--- worker.js
+++ worker.js
@@ -2,6 +2,14 @@ var detect = require('strider-detection-rules')
 var fs = require('fs')
 var path = require('path')
 
+function shellCommand(command) {
+  if (!command.replace(/#[^\n]*/g, '').trim().length) return
+  return {
+    command: 'sh',
+    args: ['-x', '-c', command]
+  }
+}
+
 module.exports = {
   init: function (config, job, context, done) {
     var venvDir = path.join(context.baseDir, '.venv')
@@ -9,25 +17,25 @@ module.exports = {
     if (config && config.test !== 'none') {
       test = config.test
     }
-    var defaultPrepare = "pip install -r requirements.txt"
+    var defaultPrepare = function (context, done) {
+        if (!fs.existsSync(path.join(context.dataDir, 'requirements.txt'))) {
+          // skip if default and no requirements.txt exists
+          // we assume that if you're configuring your own, you'll ensure the file exists
+          return done(null, true)
+        }
+        context.cmd("pip install -r requirements.txt", function (err) {
+          done(err, true)
+        })
+    }
     var prepare = defaultPrepare
     if (config && config.prepare) {
-      prepare = config.prepare
+      prepare = shellCommand(config.prepare)
     }
     done(null, {
       path: [path.join(__dirname, 'thirdparty'),
              path.join(venvDir, 'bin')],
       environment: 'virtualenv.py ' + venvDir,
-      prepare: function (context, done) {
-        if (prepare === defaultPrepare && !fs.existsSync(path.join(context.dataDir, 'requirements.txt'))) {
-          // skip if default and no requirements.txt exists
-          // we assume that if you're configuring your own, you'll ensure the file exists
-          return done(null, true)
-        }
-        context.cmd(prepare, function (err) {
-          done(err, true)
-        })
-      },
+      prepare: prepare,
       test: test
     })
   },
