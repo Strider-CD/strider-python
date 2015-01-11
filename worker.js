41ad9013201c2b44301ec0d65caddff4f079c165
Ilya Radchenko <ilya@burstcreations.com>
2015-01-10 19:31:22 -0500

    Remove unused require, cleanup

--- worker.js
+++ worker.js
@@ -1,42 +1,52 @@
-var detect = require('strider-detection-rules')
-var fs = require('fs')
-var path = require('path')
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
 
 function shellCommand(command) {
-  if (!command || !command.replace(/#[^\n]*/g, '').trim().length) return
+  if (!command || !command.replace(/#[^\n]*/g, '').trim().length) {
+    return;
+  }
+  
   return {
     command: 'sh',
     args: ['-x', '-c', command]
-  }
+  };
 }
 
 module.exports = {
   init: function (config, job, context, done) {
-    var venvDir = path.join(context.baseDir, '.venv')
-    var test = undefined
+    var venvDir = path.join(context.baseDir, '.venv');
+    var test;
+    
     if (config && config.test !== 'none') {
       test = config.test
     }
+    
     var defaultPrepare = function (context, done) {
         if (!fs.existsSync(path.join(context.dataDir, 'requirements.txt'))) {
           // skip if default and no requirements.txt exists
           // we assume that if you're configuring your own, you'll ensure the file exists
-          return done(null, true)
+          return done(null, true);
         }
-        context.cmd("pip install -r requirements.txt", function (err) {
-          done(err, true)
-        })
+        
+        context.cmd('pip install -r requirements.txt', function (err) {
+          done(err, true);
+        });
     }
-    var prepare = defaultPrepare
+    
+    var prepare = defaultPrepare;
+    
     if (config && config.prepare) {
-      prepare = shellCommand(config.prepare)
+      prepare = shellCommand(config.prepare);
     }
+    
     done(null, {
       path: [path.join(venvDir, 'bin')],
       environment: 'virtualenv ' + venvDir,
       prepare: prepare,
       test: test
-    })
+    });
   },
   autodetect: {
     filename: 'requirements.txt',
@@ -44,4 +54,4 @@ module.exports = {
     language: 'python',
     framework: null
   }
-}
+};
